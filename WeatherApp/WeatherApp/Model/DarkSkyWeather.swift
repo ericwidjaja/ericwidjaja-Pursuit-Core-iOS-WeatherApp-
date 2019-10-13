5bb2dd027f77c1bf5553c1c45c74d2a365247b45
Eric Widjaja <ericwidjaja@pursuit.org>
2019-10-12 23:49:23 -0400

    Created Model Files from Pixabay and DarkSky. Added Unit Test

---
+++ WeatherApp/WeatherApp/Model/DarkSkyWeather.swift
@@ -0,0 +1,44 @@
+//
+//  DarkSkyWeather.swift
+//  WeatherApp
+//
+//  Created by Eric Widjaja on 10/12/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+// MARK: - Weather Model
+struct DarkSkyWeather: Codable {
+    let timezone: String
+    let daily: Daily
+}
+
+enum Icon: String, Codable {
+    case cloudy = "cloudy"
+    case partlyCloudyNight = "partly-cloudy-night"
+    case rain = "rain"
+}
+
+// MARK: - Daily
+struct Daily: Codable {
+    let summary: String
+    let icon: Icon
+    let data: [DailyDatum]
+}
+
+// MARK: - DailyDatum
+struct DailyDatum: Codable {
+    let time: Int
+    let summary, icon: String
+    let sunriseTime, sunsetTime: Int
+    let precipIntensity:Double
+    let precipProbability: Double
+    let precipType: Icon?
+    let temperatureHigh: Double
+    let temperatureLow: Double
+    let windSpeed: Double
+    let temperatureMinTime: Int
+    let temperatureMax: Double
+    let temperatureMaxTime: Int
+}
