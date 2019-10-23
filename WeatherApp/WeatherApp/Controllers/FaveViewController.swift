7ac0d0a7415cebfe0733218fd3f305473bc7c49d
Eric Widjaja <ericwidjaja@pursuit.org>
2019-10-22 22:23:51 -0400

    Added Fave View Controller, set up its IBOutlet

--- WeatherApp/WeatherApp/Controllers/FaveViewController.swift
+++ WeatherApp/WeatherApp/Controllers/FaveViewController.swift
@@ -9,22 +9,36 @@
 import UIKit
 
 class FaveViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    var favorites = [FavePict]() {
+        didSet {
+            faveTableView.reloadData()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var faveTableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
     }
-    */
+}
 
+extension FaveViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = faveTableView.dequeueReusableCell(withIdentifier: "faveCell", for: indexPath) as! FavoTableViewCell
+        let fav = favorites[indexPath.row]
+        cell.cityLabel.text = fav.name
+        cell.cityImage.image = UIImage(data: fav.image)
+        cell.delegate = self as! FavDelegate
+        return cell
+    }
 }
